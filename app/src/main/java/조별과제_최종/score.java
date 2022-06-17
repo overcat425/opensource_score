934e66604c1cd3d4fb63e5cfa5fd8b25a52a44c9
Kim Bomi <bxm1kim@gmail.com>
2022-06-17 15:30:35 +0900

    grade class 구현

---
+++ app/src/main/java/조별과제_최종/score.java
@@ -0,0 +1,40 @@
+public class package grade {
+    
+    int studentId; // 학번
+    Subject subject; //과목
+    int point; //점수
+
+    public Score(int studentId, Subject subject, int point) {
+        this.studentId=studentId;
+        this.subject=subject;
+        this.point=point;
+    }
+
+    public int getStudentId(){
+        return studentId;
+    }
+
+    public void setStudentId(int studentId){
+        this.studentId=studentId;
+    }
+
+    public Subject getSubject( ){
+        return subject;
+    }
+
+    public void setSubject(Subject subject){
+        this.subject=subject;
+    }
+
+    public int getPoiny( ){
+        return point;
+    }
+
+    public void setPoint(int point){
+        this. point=point;
+    }
+
+    public String toString( ){
+        return "학번:"+studentId+","+subject.getSubjectName( )+":"+point; //toString()메서드 재정의
+    }
+}
